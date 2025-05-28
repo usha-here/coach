a4b7c21afb4e86604cfddd6cb38c9103e0d419ab
usha here <ushanitwal09@gmail.com>
2025-05-28 21:12:20 +0530

    implemented clerk authorization

--- components/header.jsx
+++ components/header.jsx
@@ -1,10 +1,25 @@
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
+    <header>         
+        <nav>
+            <Link href="/">
+            <Image src='/coachlogo.png' alt='logo' width={200} height={60}
+            className='h-12 py-1 w-auto object-contain'/>
+            </Link>
+        </nav>
+      <SignedOut>    
+              <SignInButton />    
+              <SignUpButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton/>
+            </SignedIn>
+    </header>
   )
 }
 
