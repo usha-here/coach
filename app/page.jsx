7e0714c990b0b896e571be041f9edc9f8e4b041a
usha here <ushanitwal09@gmail.com>
2025-06-04 23:47:32 +0530

    refactor HeroSection component and enhance layout in Home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,14 @@
 import HeroSection from "@/components/hero";
 import { Button } from "@/components/ui/button";
 
+
 export default function Home() {
   return (
     <div>
        <div className="grid-background">
        </div>
        <HeroSection/>
+        
     </div>
       );
 }
--- components/hero.jsx
+++ components/hero.jsx
@@ -2,10 +2,17 @@ import React from 'react'
 
 const HeroSection = () => {
   return (
-    <div>
-    HeroSection
-    </div>
+    <section className='w-full pt-36 md:pt-48 pb-10'>
+      <div>
+        <div>
+          <h1>
+            Your AI Career Coach for  <br/>
+            Professional Success. <br/>
+            COACH : Creating Opportunities And Career Hope
+          </h1>
+        </div>
+      </div>
+    </section>
   )
 }
-
 export default HeroSection
